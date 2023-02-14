90903aa7b5e778d4fc9b1fb9f9c2d4b51539f3b6
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-02-13 15:27:46 -0900

    G, completed function

--- WhiteboardPrompts/SquaresOfASortedArray.js
+++ WhiteboardPrompts/SquaresOfASortedArray.js
@@ -1,4 +1,8 @@
 // The Straightforward approach, not caring about time complexity
 function SquaresOfASortedArray(array) {
- return array
+	const output = []  
+	for(let i = 0; i < array.length; i++){
+		output.push(array[i] * array[i])
+	}
+	return output.sort((a, b) => a-b)
 }
