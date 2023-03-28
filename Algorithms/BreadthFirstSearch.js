600cf9544445ea55e8f7651153649aa5295f1da9
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-03-28 09:30:00 -0900

    solution

--- Algorithms/BreadthFirstSearch.js
+++ Algorithms/BreadthFirstSearch.js
@@ -25,3 +25,22 @@ c.right = f;
 //     b      c
 //   /   \     \
 // d      e     f
+
+const breadthFirstValues = (root) => {
+  if (root === null) return [];
+
+  const values = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    values.push(current.val);
+
+    if (current.left !== null) queue.push(current.left);
+    if (current.right !== null) queue.push(current.right);
+  }
+
+  return values;
+};
+
+breadthFirstValues(a);
