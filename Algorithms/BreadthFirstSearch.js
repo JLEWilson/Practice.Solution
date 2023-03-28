263167d550363f16f381591da5f99f84fc48234a
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-03-28 09:35:00 -0900

    practice 1

--- Algorithms/BreadthFirstSearch.js
+++ Algorithms/BreadthFirstSearch.js
@@ -44,3 +44,17 @@ const breadthFirstValues = (root) => {
 };
 
 breadthFirstValues(a);
+
+const breadthFirstSearch1 = (root, target) => {
+  if (root === null) return false;
+
+  const queue = [root];
+  while (queue.length > 0) {
+    const current = queue.shift();
+    if (current.val === target) return true;
+
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+  return false;
+};
